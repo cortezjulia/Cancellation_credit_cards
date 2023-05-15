9a586de0548c9a45170e173b76b1b8b3a8a6ffd1
Julia Cortez <jcgaucha@gmail.com>
2023-05-15 17:52:56 -0300

    generated graphics

--- repo/Cancellation_credit_cards.py
+++ repo/Cancellation_credit_cards.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from IPython.display import display
+import plotly.express as px
 
 table=pd.read_csv('ClientesBanco.csv',encoding='latin1')
 table=table.drop('CLIENTNUM',axis=1)
@@ -16,4 +17,9 @@ category_quantity=table['Categoria'].value_counts()
 display(category_quantity)
 category_quantity_perc=table['Categoria'].value_counts(normalize=True)
 display(category_quantity_perc)
+
+for column in table:
+    grafico=px.histogram(table,x=column,color='Categoria')
+    grafico.show()
+
 
